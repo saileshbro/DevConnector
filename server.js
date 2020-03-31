fa9e94ebec5e47ba1f55b8456921ba7a9670c3f1
Sailesh Dahal <saileshbro@gmail.com>
2020-03-31 17:57:47 +0545

    Created config file and mongoose connection

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const connectDB = require('./config/db')
 const app = express()
+// Connect Database
+connectDB()
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('API RUNNING'))
