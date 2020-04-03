e71e50e3c9ccf6b5f1ff93f092c91f82d6736083
Sailesh Dahal <saileshbro@gmail.com>
2020-04-03 16:13:30 +0545

    added 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,12 @@ import './App.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
-import Alert from './components/layout/Alert'
 // redux
 import { Provider } from 'react-redux'
 import store from './store'
 import setAuthToken from './utils/setAuthToken'
 import { loadUser } from './actions/auth'
-import Dashboard from './components/dashboard/Dashboard'
-import PrivateRoute from './components/routing/PrivateRoute'
-import CreateProfile from './components/profile-form/CreateProfile'
-import EditProfile from './components/profile-form/EditProfile'
-import AddExperience from './components/profile-form/AddExperience'
-import AddEducation from './components/profile-form/AddEducation'
-import Profiles from './components/profiles/Profiles'
-import Profile from './components/profile/Profile'
-import Posts from './components/posts/Posts'
-import Post from './components/post/Post'
+import { Routes } from './components/routing/Routes'
 
 if (localStorage.token) {
 	setAuthToken(localStorage.token)
@@ -34,35 +22,10 @@ const App = () => {
 		<Provider store={store}>
 			<Router>
 				<Navbar />
-				<Route exact path='/' component={Landing} />
-				<section className='container'>
-					<Alert />
-					<Switch>
-						<Route exact path='/register' component={Register} />
-						<Route exact path='/login' component={Login} />
-						<Route exact path='/profiles' component={Profiles} />
-						<Route exact path='/profile/:id' component={Profile} />
-						<PrivateRoute exact path='/dashboard' component={Dashboard} />
-						<PrivateRoute
-							exact
-							path='/create-profile'
-							component={CreateProfile}
-						/>
-						<PrivateRoute exact path='/edit-profile' component={EditProfile} />
-						<PrivateRoute
-							exact
-							path='/add-experience'
-							component={AddExperience}
-						/>
-						<PrivateRoute exact path='/posts' component={Posts} />
-						<PrivateRoute
-							exact
-							path='/add-education'
-							component={AddEducation}
-						/>
-						<PrivateRoute exact path='/posts/:id' component={Post} />
-					</Switch>
-				</section>
+				<Switch>
+					<Route exact path='/' component={Landing} />
+					<Route component={Routes} />
+				</Switch>
 			</Router>
 		</Provider>
 	)
